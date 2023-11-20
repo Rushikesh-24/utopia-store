ff645799f999818f78d7f154d23480d2b14c3284
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2023-11-20 09:23:48 +0530

    updated env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 export const authOptions = {
     providers:[
        GoogleProvider({
-        clientId:process.env.CLIENT_ID,
-        clientSecret:process.env.CLIENT_SECRET,
+        clientId:'635547955951-sgs0qingksnff5endrnr2r7fn4fu6spk.apps.googleusercontent.com',
+        clientSecret:'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',
        }) 
     ]
 }
